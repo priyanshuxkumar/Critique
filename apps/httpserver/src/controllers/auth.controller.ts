f0e2340fcdf3f55e8f5bb284e08ba1256d2ba666
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-03-11 16:01:17 +0530

    refactor: moved logic from user- route to user-controller

---
+++ apps/httpserver/src/controllers/auth.controller.ts
@@ -0,0 +1,115 @@
+import { Request, Response, Router } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { SigninSchema, SignupSchema } from  '../types';
+import { prisma } from 'db';
+import { JWT_SECRET } from '../config';
+
+const registerUser = async(req: Request , res: Response) => {
+    try {
+        const body = req.body;
+        const parsedData = SignupSchema.safeParse(body)
+        if(!parsedData.success){
+            res.status(400).json({message: parsedData.error.issues[0].message});
+            return;
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                email: parsedData.data.email
+            }
+        })
+
+        if(user){
+            res.status(409).json({message: 'User already exists with this email'})
+            return;
+        }
+
+        const hashPassword = await bcrypt.hash(parsedData.data.password, 10);
+
+        await prisma.user.create({
+            data: {
+                ...parsedData.data,
+                password: hashPassword
+            }
+        })
+        res.status(200).json({message: "User registered successfully!"})
+    } catch (error) {
+        res.status(500).json({message : 'Something went wrong'});   
+    }
+}
+
+const loginUser = async(req: Request , res: Response) => {
+    try {
+        const body = req.body;
+        const parsedData = SigninSchema.safeParse(body)
+        if(!parsedData.success){
+            res.status(400).json({message: parsedData.error.issues[0].message});
+            return;
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                email: parsedData.data.email
+            }
+        })
+
+        if(!user){
+            res.status(404).json({message : 'Invalid credentials'});
+            return;
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(parsedData.data.password , user.password)
+        if(!isPasswordCorrect){
+            res.status(403).json({mesasge: 'Invalid crendentials'});
+            return;
+        }
+
+        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '24h'})
+        const options = {
+            httpOnly : true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 1000 * 60 * 60 * 24,
+            sameSite: 'strict' as 'strict',
+            path: '/'
+        }
+
+        res.cookie('_token_',token, options);
+        res.status(200).json({message: 'Signin successfull!'})
+    } catch (error) {
+        res.status(500).json({message : 'Something went wrong'}); 
+    }
+}
+
+const getUser = async(req: Request , res: Response) =>  {
+    const userId = req.id as number;
+    try {
+        const user = await prisma.user.findFirst({
+            where : {
+                id : userId
+            }
+        })
+        res.status(200).json({
+                id: user?.id,
+                name : user?.name,
+                avatar: user?.avatar
+            }
+        )
+    } catch (error) {
+        res.status(500).json({message : 'Something went wrong'}); 
+    }
+}
+
+const logoutUser = async(req: Request , res: Response) =>  {
+    try {
+        res.clearCookie('_token_', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            path: '/'
+        })
+        res.status(200).json({message: 'logout successfully'});
+    } catch (error) {
+        res.status(500).json({message : 'Something went wrong'});    
+    }
+}
+
+export { registerUser, loginUser, getUser, logoutUser }
--- apps/httpserver/src/routes/auth.route.ts
+++ apps/httpserver/src/routes/auth.route.ts
@@ -1,120 +1,15 @@
-import { Request, Response, Router } from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { SigninSchema, SignupSchema } from  '../types';
-import { prisma } from 'db';
-import { JWT_SECRET } from '../config';
+import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth.middleware';
+import { registerUser , loginUser, getUser, logoutUser } from '../controllers/auth.controller';
 
 const router = Router()
 
-router.post('/signup', async (req: Request , res: Response) => {
-    try {
-        const body = req.body;
-        const parsedData = SignupSchema.safeParse(body)
-        if(!parsedData.success){
-            res.status(400).json({message: parsedData.error.issues[0].message});
-            return;
-        }
-        const user = await prisma.user.findFirst({
-            where: {
-                email: parsedData.data.email
-            }
-        })
+router.route("/signup").post(registerUser);
 
-        if(user){
-            res.status(409).json({message: 'User already exists with this email'})
-            return;
-        }
+router.route("/signin").post(loginUser);
 
-        const hashPassword = await bcrypt.hash(parsedData.data.password, 10);
+router.route("/").get(authMiddleware, getUser);
 
-        await prisma.user.create({
-            data: {
-                ...parsedData.data,
-                password: hashPassword
-            }
-        })
-
-        res.status(200).json({message: "User registered successfully!"})
-
-    } catch (error) {
-        res.status(500).json({message : 'Something went wrong'});   
-    }
-})
-
-router.post('/signin', async(req: Request , res: Response) => {
-    try {
-        const body = req.body;
-        const parsedData = SigninSchema.safeParse(body)
-        if(!parsedData.success){
-            res.status(400).json({message: parsedData.error.issues[0].message});
-            return;
-        }
-        const user = await prisma.user.findFirst({
-            where: {
-                email: parsedData.data.email
-            }
-        })
-
-        if(!user){
-            res.status(404).json({message : 'Invalid credentials'});
-            return;
-        }
-
-        const isPasswordCorrect = await bcrypt.compare(parsedData.data.password , user.password)
-        if(!isPasswordCorrect){
-            res.status(403).json({mesasge: 'Invalid crendentials'});
-            return;
-        }
-
-        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '24h'})
-        const options = {
-            httpOnly : true,
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 1000 * 60 * 60 * 24,
-            sameSite: 'strict' as 'strict',
-            path: '/'
-        }
-
-        res.cookie('_token_',token, options);
-        res.status(200).json({message: 'Signin successfull!'})
-    } catch (error) {
-        res.status(500).json({message : 'Something went wrong'}); 
-    }
-})
-
-router.get('/', authMiddleware , async(req: Request , res: Response) => {
-    const userId = req.id as number;
-    try {
-        const user = await prisma.user.findFirst({
-            where : {
-                id : userId
-            }
-        })
-        res.status(200).json({
-                id: user?.id,
-                name : user?.name,
-                avatar: user?.avatar
-            }
-        )
-    } catch (error) {
-        res.status(500).json({message : 'Something went wrong'}); 
-    }
-})
-
-router.post('/logout', authMiddleware, async(req: Request , res: Response) => {
-    try {
-        res.clearCookie('_token_', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            path: '/'
-        })
-        res.status(200).json({message: 'logout successfully'});
-    } catch (error) {
-        res.status(500).json({message : 'Something went wrong'});    
-    }
-})
+router.route('/logout').post(logoutUser)
 
 export const authRouter = router;
