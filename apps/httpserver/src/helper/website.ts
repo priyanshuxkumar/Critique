87904c5816efc0aa881214e67b91022b40671781
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-03-11 23:09:36 +0530

    refactor checkIsWebsite func

--- apps/httpserver/src/helper/website.ts
+++ apps/httpserver/src/helper/website.ts
@@ -1,21 +1,29 @@
-async function checkIsWebsiteExist (url : string)  {
+const FETCH_TIMEOUT_MS = 5000;
+
+async function checkIsWebsiteExist (url : string) : Promise<boolean>  {
+    try {
+        new URL(url);
+    } catch (e) {
+        throw new Error("Invalid URL format");
+    }
+
     const controller = new AbortController();
-    const timeout = setTimeout(() => controller.abort(), 5000);
+    const timeout = setTimeout(() => controller.abort(), FETCH_TIMEOUT_MS);
     try {
         const response = await fetch(url, {
             method: 'HEAD',
             signal :  controller.signal
         });
-        if(response.ok) {
-            return true;
+        return response.ok;
+    } catch (error : unknown) {
+        if (error instanceof Error) {
+            if (error.name === "AbortError") {
+                throw new Error(`Request timed out after ${FETCH_TIMEOUT_MS * 1000}s`);
+            } else {
+                throw new Error(`Failed to verify website`);
+            }
         }
-        return false;
-    } catch (error : any) {
-        if (error.name === "AbortError") {
-            throw new Error("Request timed out.");
-        } else {
-            throw new Error("Error verifying website. Please try again later.")
-        }
+        throw new Error("Something went wrong!");
     } finally {
         clearTimeout(timeout);
     }
