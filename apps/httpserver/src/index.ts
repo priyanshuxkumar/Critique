d5c2769db82c81b92909169bb6d8f47d0456cb28
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-06 02:47:55 +0530

    add route

--- apps/httpserver/src/index.ts
+++ apps/httpserver/src/index.ts
@@ -1,29 +1,31 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import { authRouter } from './routes/auth.route';
-import { websiteRouter } from './routes/website.route';
-import { reviewRouter } from './routes/review.route';
-import { upvoteRouter } from './routes/upvote.route';
-import { emailRouter } from './routes/email.route';
-import { config } from './config';
+import { authRouter } from "./routes/auth.route";
+import { websiteRouter } from "./routes/website.route";
+import { reviewRouter } from "./routes/review.route";
+import { upvoteRouter } from "./routes/upvote.route";
+import { emailRouter } from "./routes/email.route";
+import { leaderboardRouter } from "./routes/leaderboard.route";
+import { config } from "./config";
 
 export const app = express();
 
 const corsOptions = {
-    origin: config.frontendUrl,
-    method: ['GET','POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}
+  origin: config.frontendUrl,
+  method: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/website', websiteRouter);
-app.use('/api/v1/review', reviewRouter);
-app.use('/api/v1/upvote', upvoteRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/website", websiteRouter);
+app.use("/api/v1/review", reviewRouter);
+app.use("/api/v1/upvote", upvoteRouter);
+app.use("/api/v1/leaderboard", leaderboardRouter);
 app.use(emailRouter);
