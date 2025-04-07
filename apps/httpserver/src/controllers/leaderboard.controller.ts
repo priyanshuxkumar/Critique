154b1dac425d2beeb623e197bc6287a3b61088e4
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-07 20:21:23 +0530

    update leaderboard controller

--- apps/httpserver/src/controllers/leaderboard.controller.ts
+++ apps/httpserver/src/controllers/leaderboard.controller.ts
@@ -4,7 +4,32 @@ import { calculateLeaderboard } from "../helper/leaderboard";
 
 const getLeaderboard = async (req: Request, res: Response) => {
   try {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
     const result = await prisma.website.findMany({
+      where: {
+        reviews: {
+          some: {
+            OR: [
+              {
+                createdAt: {
+                  gte: oneWeekAgo,
+                },
+              },
+              {
+                upvotes: {
+                  some: {
+                    createdAt: {
+                      gte: oneWeekAgo,
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
       include: {
         reviews: {
           include: {
@@ -15,13 +40,28 @@ const getLeaderboard = async (req: Request, res: Response) => {
     });
     const data = calculateLeaderboard(result);
 
-    const leaderboard = data.sort(
-      (a: any, b: any) => b.rankingScore - a.rankingScore
-    );
+    const websites = data.sort((a: any, b: any) => b.rankingScore - a.rankingScore).slice(0,10);
 
-    res.status(200).json(leaderboard);
+    res.status(200).json(
+      websites.map((item: any) => ({
+        id: item.id, 
+        name: item.name,
+        websiteUrl: item.websiteUrl,
+        iconUrl: item.iconUrl,
+        totalReviews: item.totalReviews,
+        avgRating: item.avgRating,
+        totalReviewsUpvotes: item.totalReviewsUpvotes,
+        rankingScore: item.rankingScore,
+        isVerified: item.isVerified,
+        category: item.category,
+      }))
+    );
   } catch (error) {
-    console.error(error);
+    if(error instanceof Error) {
+        res.status(500).json({ message: error.message });
+        return;
+    }
+    res.status(500).json({ message: 'Something went wrong' });
   }
 };
 
